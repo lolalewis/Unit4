973095eb1b3958f7d7ce9461eb5413d4dfc9ea81
lola1590 <lola1590@ISS2TWRKH2.scdsb.pri>
2019-11-08 11:58:59 -0500

    updated from school Nov.8/19

---
+++ src/Lesson10SearchSortInsert/ISSStudent.java
@@ -0,0 +1,29 @@
+package Lesson10SearchSortInsert;
+
+
+public class ISSStudent implements Comparable{
+    private String name,address;
+    private int id;
+    
+    public ISSStudent (String n, String a, int i){
+        name = n;
+        address=a;
+        id=i;
+    }
+    public String toString(){
+        String str  = "Name: "+ name;
+        str+= "\nAddress: " + address;
+        str+="\nID: " + id + "\n--------";
+        return str;
+    }
+    public int getID(){
+        return id;
+    }
+
+    @Override
+    public int compareTo(Object t) {
+      int otherid = ((ISSStudent)t).getID();
+      return id - otherid;
+    }
+    
+}
