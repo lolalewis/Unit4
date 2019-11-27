90cfb059c629f96609388a433a0c4a9601a8da46
lola1590 <lola1590@ISS2TWRKH2.scdsb.pri>
2019-11-27 11:58:26 -0500

    updated work from school Nov.27/19

--- src/Lesson11Recursion/Hanoi.java
+++ src/Lesson11Recursion/Hanoi.java
@@ -6,7 +6,8 @@ public class Hanoi {
     static int nummoves=0;
     
     public static void main(String[]args){
-       
+      // KeyboardReader k = new KeyboardReader();
+      // int numrings = k.readInt("Enter a number of rings");
         
     }
 }
